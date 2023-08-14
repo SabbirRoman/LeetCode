fb8f53a29d3fbc57bf4fafe2e678736f75e21bbf
Roman_Emper0r <95046730+SabbirRoman@users.noreply.github.com>
2023-08-14 15:53:35 +0600

    Time: 4 ms (91.19%), Space: 9.3 MB (54.08%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n=boxes.size();
-        vector<int> a,b;
+        vector<int> a;
         int tmp=0, cnt_one=0;
         for(int i=0; i<n; i++)
         {
@@ -15,16 +15,11 @@ public:
         tmp=0, cnt_one=0;
         for(int i=n-1; i>=0; i--)
         {
-            b.push_back(tmp+cnt_one);
+            a[i]+=(tmp+cnt_one);
             tmp=tmp+cnt_one;
             if(boxes[i]=='1') cnt_one++;
         }
-        // vector "b" store the results in reverse order
-       for(int i=0; i<n; i++)
-        {
-           a[i]+=b[n-(i+1)];
-        }
-
+        
         return a;
     }
 };
