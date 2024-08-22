96c19634ec252c6195e9a1f5288e74b4a6e31de0
Roman_Emper0r <95046730+SabbirRoman@users.noreply.github.com>
2024-08-23 03:02:18 +0600

    Time: 3 ms (41.26%), Space: 7.7 MB (8.45%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -4,22 +4,19 @@ public:
         string s;
         while(num)
         {
-            if(num&1) s+='1';
-            else s+='0';
-            num/=2;
+            if(num%2==0) s +='1';
+            else s +='0';
+            num /= 2;
         }
-        int n=s.size();
-        int ans=0;
-        long long int tmp=1;
-        
-
-        for(int i=0; i<n; i++)
+        int ans = 0;
+        long long tmp = 1;
+        for(int i = 0; i<s.size(); i++)
         {
-            if(s[i]=='0') ans+=tmp;
             
-            tmp*=2;
+            if(s[i]=='1') ans += tmp;
+            tmp *= 2;
         }
-        
         return ans;
+
     }
 };
